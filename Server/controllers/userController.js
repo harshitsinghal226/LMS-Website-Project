26af3c52d79d89dd2c5e21ef1fd12c676c1bc0a4
harshitsinghal226 <harshitsinghal226@gmail.com>
2025-08-26 01:53:18 +0530

    backend completed

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -31,57 +31,150 @@ export const userEnrolledCourses = async (req, res) => {
   }
 };
 
-
 // Purchase Course
 export const purchaseCourse = async (req, res) => {
-    try {
-        const { courseId } = req.body;
-        const { origin } = req.headers;
-        const userId = req.auth.userId;
-        const userData = await User.findById(userId);
-        const courseData = await Course.findById(courseId);
+  try {
+    const { courseId } = req.body;
+    const { origin } = req.headers;
+    const userId = req.auth.userId;
+    const userData = await User.findById(userId);
+    const courseData = await Course.findById(courseId);
 
-        if(!userData || !courseData) {
-            return res.json({ success: false, message: "Data not Found"});
-        }
-
-        const purchaseData = {
-            courseId: courseData._id,
-            userId,
-            amount: (courseData.price- courseData.discount * courseData.coursePrice/100).toFixed(2),
-        }
-
-        const newPurchase = await Purchase.create(purchaseData);
-
-        //Stripe Gateway Initialization
-        const stripeInstance = new Stripe(process.env.STRIPE_SECRET_KEY);
-        const currency = process.env.CURRENCY.toLowerCase()
-
-        // Creating line items to for stripe
-        const line_items = [{
-            price_data: {
-                currency,
-                product_data: {
-                    name: courseData.courseTitle
-                },
-                unit_amount: Math.floor(newPurchase.amount)*100
-            },
-            quantity: 1
-        }]
-
-        const session = await stripeInstance.checkout.sessions.create({
-            success_url: `${origin}/loading/my-enrollments`,
-            cancel_url: `${origin}/`,
-            line_items: line_items,
-            mode: 'payment',
-            metadata: {
-                purchaseId: newPurchase._id.toString()
-            }
-        })
-
-        res.json({ success: true, session_url: session.url });
-
-    } catch(error) {
-        res.json({ success: false, message: error.message });
+    if (!userData || !courseData) {
+      return res.json({ success: false, message: "Data not Found" });
     }
+
+    const purchaseData = {
+      courseId: courseData._id,
+      userId,
+      amount: (
+        courseData.price -
+        (courseData.discount * courseData.coursePrice) / 100
+      ).toFixed(2),
+    };
+
+    const newPurchase = await Purchase.create(purchaseData);
+
+    //Stripe Gateway Initialization
+    const stripeInstance = new Stripe(process.env.STRIPE_SECRET_KEY);
+    const currency = process.env.CURRENCY.toLowerCase();
+
+    // Creating line items to for stripe
+    const line_items = [
+      {
+        price_data: {
+          currency,
+          product_data: {
+            name: courseData.courseTitle,
+          },
+          unit_amount: Math.floor(newPurchase.amount) * 100,
+        },
+        quantity: 1,
+      },
+    ];
+
+    const session = await stripeInstance.checkout.sessions.create({
+      success_url: `${origin}/loading/my-enrollments`,
+      cancel_url: `${origin}/`,
+      line_items: line_items,
+      mode: "payment",
+      metadata: {
+        purchaseId: newPurchase._id.toString(),
+      },
+    });
+
+    res.json({ success: true, session_url: session.url });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// Update User Course Progress
+export const updateUserCourseProgress = async (req, res) => {
+  try {
+    const userId = req.auth.userId;
+    const { courseId, lectureId } = req.body;
+
+    const progressData = await CourseProgress.findOne({ userId, courseId });
+
+    if (progressData) {
+      if (progressData.lectureCompleted.includes(lectureId)) {
+        return res.json({
+          success: true,
+          message: "Lecture already completed",
+        });
+      }
+
+      progressData.lectureCompleted.push(lectureId);
+      await progressData.save();
+    }
+    else {
+      await CourseProgress.create({
+        userId,
+        courseId,
+        lectureCompleted: [lectureId],
+      });
+    }
+
+    res.json({ success: true, message: "Course Progress Updated" });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// Get User Course Progress
+export const getUserCourseProgress = async (req, res) => {
+  try {
+    const userId = req.auth.userId;
+    const { courseId } = req.body;
+
+    const progressData = await CourseProgress.findOne({ userId, courseId });
+
+    if (!progressData) {
+      return res.json({ success: false, message: "No progress found" });
+    }
+
+    res.json({ success: true, progressData });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+}
+
+// Add User Ratings to Course
+export const addUserRating = async (req, res) => {
+  try {
+    const userId = req.auth.userId;
+    const {courseId, rating} = req.body;
+
+    if(!courseId || !userId || !rating || rating< 1 || rating>5) {
+      return res.json({success: false, message: "Invalid Data"});
+    }
+
+    const course = await Course.findById(courseId);
+
+    if(!course) {
+      return res.json({success: false, message: "Course not found"});
+    }
+
+    const user = await User.findById(userId);
+
+    if(!user || !user.enrolledCourses.includes(courseId)) {
+      return res.json({success: false, message: "User not found"});
+    }
+
+    const existingRatingIndex = course.courseRatings.findIndex(r => r.userId === userId);
+
+    if(existingRatingIndex > -1) {
+      course.courseRatings[existingRatingIndex].rating = rating;
+    } else {
+      course.courseRatings.push({userId, rating});
+    }
+
+    await course.save();
+    res.json({success: true, message: "Rating added successfully"});
+
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+    
+  }
 }

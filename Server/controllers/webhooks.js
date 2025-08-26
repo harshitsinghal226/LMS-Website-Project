cb4fd3c8b8d86e07083c92285004e3114be8521c
harshitsinghal226 <harshitsinghal226@gmail.com>
2025-08-26 14:41:20 +0530

    changes in clerk webhooks

--- Server/controllers/webhooks.js
+++ Server/controllers/webhooks.js
@@ -10,52 +10,44 @@ export const clerkWebhooks = async (req, res) => {
   try {
     const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
 
-    // Verify raw body
-    await whook.verify(req.body, {
+    // svix expects raw body as string or buffer
+    whook.verify(req.body, {
       "svix-id": req.headers["svix-id"],
       "svix-timestamp": req.headers["svix-timestamp"],
       "svix-signature": req.headers["svix-signature"],
     });
 
+    // Convert raw buffer to string, then parse JSON
     const { data, type } = JSON.parse(req.body.toString());
 
     switch (type) {
-      case "user.created": {
-        const userData = {
+      case "user.created":
+        await User.create({
           _id: data.id,
           email: data.email_addresses[0].email_address,
-          name: data.first_name + " " + data.last_name,
+          name: `${data.first_name} ${data.last_name}`,
           imageUrl: data.image_url,
-        };
+        });
+        return res.status(200).send();
 
-        // to save this data in our database
-        await User.create(userData);
-        res.json({});
-        break;
-      }
-
-      case "user.updated": {
-        const userData = {
+      case "user.updated":
+        await User.findByIdAndUpdate(data.id, {
           email: data.email_addresses[0].email_address,
-          name: data.first_name + " " + data.last_name,
+          name: `${data.first_name} ${data.last_name}`,
           imageUrl: data.image_url,
-        };
-        await User.findByIdAndUpdate(data.id, userData);
-        res.json({});
-        break;
-      }
+        });
+        return res.status(200).send();
 
-      case "user.deleted": {
+      case "user.deleted":
         await User.findByIdAndDelete(data.id);
-        res.json({});
-        break;
-      }
+        return res.status(200).send();
 
       default:
-        break;
+        return res.status(200).send(); // acknowledge other events
     }
   } catch (error) {
-    res.json({ success: false, message: error.message });
+    console.error("Clerk Webhook Error:", error);
+    return res.status(400).send(`Webhook Error: ${error.message}`);
   }
 };
 

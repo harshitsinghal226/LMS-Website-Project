73e323e81df98bdf2e5a871fee9787b66275299b
harshitsinghal226 <harshitsinghal226@gmail.com>
2025-08-26 14:20:23 +0530

    added try catch block in mongodb

--- Server/configs/mongodb.js
+++ Server/configs/mongodb.js
@@ -1,9 +1,16 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  mongoose.connection.on("connected", () => console.log("Database Connected"));
+  try {
+    mongoose.connection.on("connected", () => 
+      console.log("✅ Database Connected")
+    );
 
-  await mongoose.connect(`${process.env.MONGODB_URI}/lms`);
+    await mongoose.connect(`${process.env.MONGODB_URI}/lms`);
+  } catch (error) {
+    console.error("❌ MongoDB connection error:", error.message);
+    process.exit(1);
+  }
 };
 
 export default connectDB;
